00362653a2e836c146f321f0f3f5dc6b6a8c4838
udsoncosta <udsoncostasantana@gmail.com>
2023-10-20 12:56:01 -0300

    Criando Function com tipagem fixa

---
+++ Exercícios/function.ts
@@ -0,0 +1,17 @@
+function profissao(nome: string, funcao: string){
+    console.log(nome + ', ' + funcao)
+}
+profissao('Udson', 'Desenvolvedor Web FullStack')
+
+
+function soma(num1: number, num2: number){
+console.log(num1 + num2)
+}
+
+soma(10,14)
+ 
+function endereco(rua: string, numero: number){
+    console.log(rua + ', ' + numero)
+}
+
+endereco('Avenida Bandeirantes', 24)
